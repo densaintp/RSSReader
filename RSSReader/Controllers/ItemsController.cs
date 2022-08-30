e25b3b9d69f0cfca82776977148f74777242538b
densaintp <73133105+densaintp@users.noreply.github.com>
2022-08-30 22:16:25 +0300

    Добавьте файлы проекта.

---
+++ RSSReader/Controllers/ItemsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RSSReader.Models;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml;
+using System.Text.RegularExpressions;
+
+namespace RSSReader.Controllers
+{
+	public class ItemsController : Controller
+	{
+        public IActionResult Index()
+		{
+            return View();
+		}
+
+        [HttpPost]
+        public ActionResult Index(string RssUrl)
+        {
+            HttpClient hclient = new HttpClient();
+            string RSSData = hclient.GetStringAsync(RssUrl).Result;
+
+            XDocument xml = XDocument.Parse(RSSData);
+                var RSSFeedData = (from i in xml.Descendants("item") select new Item
+                                   {
+                                       Title = (string)i.Element("title"),
+                                       Url = (string)i.Element("link"),
+                                       Description = Regex.Replace((string)i.Element("description").Value, 
+                                       @"<[^>]*(>|$)|&nbsp;|&zwnj;|&raquo;|&laquo;", string.Empty).Trim(),
+                                       PostDate = (DateTime)i.Element("pubDate")
+                                   });
+            ViewBag.RSSFeed = RSSFeedData;
+            ViewBag.URL = RssUrl;
+            return View();
+        }
+    }
+}
---
+++ RSSReader/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace RSSReader.Models
+{
+	public class Item
+	{
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Url { get; set; }
+		public DateTime PostDate  { get; set; }
+
+	}
+}
